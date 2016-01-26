5fcc9b0ad75e4de6bafa2099baffc55dae5c33e0
Kyle Derrick and Mao Thygesen <maothygesen@gmail.com>
2016-01-25 16:17:46 -0800

    add js to add form info

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,26 @@ function toDo (task, timeOfDay, frequency, timeLength) {
   this.frequency = frequency;
   this.timeLength = timeLength;
 };
-toDo.prototype.taskFrequency = function(task, frequency) {
+toDo.prototype.taskFrequency = function() {
   return this.task + ", " + this.frequency;
 };
 
-$(document).ready(function(event) {
+$(document).ready(function() {
+  $("form#new-toDo").submit(function(event){
+    event.preventDefault();
 
-})
+    var inputTask = $("#new-task").val();
+    var inputTime = $("#new-timeOfDay").val();
+    var inputFrequency = $("#new-frequency").val();
+    var inputLength = $("#new-timeLength").val();
+    var newToDo = new toDo(inputTask, inputTime, inputFrequency, inputLength);
+
+    $("ol#list").append("<li><span class='onList'>" + newToDo.taskFrequency() + ", " + newToDo.timeOfDay +", "+ newToDo.timeLength + "</span>");
+      $(".onList").dblclick(function(){
+
+      });
+
+    $("input").val("");
+
+  });
+});
